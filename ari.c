bb69febd696d0c7f13b700d9ac45744d3af1da54
SreejithB190841EC <84890942+SreejithB190841EC@users.noreply.github.com>
2021-06-29 20:27:23 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -13,6 +13,33 @@ string ari(string a)
    nw=(int)sc;
    if((sc-nw)!=0)
        nw++;
-   string s[]={"fgg","Kindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eight Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth Grade","College student","Professor"};
-   return s[nw];
+   if(nw==1)
+     a="Kindergarten";
+   else if(nw==2)
+     a="First/Second Grade";
+   else if(nw==3)
+     a="Third Grade";
+   else if(nw==4)
+     a="Fourth Grade";
+    else if(nw==5)
+     a="Fifth Grade";
+    else if(nw==6)
+     a="Sixth Grade";
+    else if(nw==7)
+     a="Seventh Grade";
+    else if(nw==8)
+     a="Eight Grade";
+    else if(nw==9)
+     a="Ninth Grade";
+     else if(nw==10)
+     a="Tenth Grade";
+    else if(nw==11)
+     a="Eleventh Grade";
+    else if(nw==12)
+     a="Twelfth Grade";
+    else if(nw==13)
+     a="College student";
+    else if(nw==14)
+     a="Professor";
+   return a;
   }
